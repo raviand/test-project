99bf31c785eb4fa367d1d004e3997b7ef1e414f9
Ramiro Videka <rvidela@lemongrasscloud.com>
2024-10-02 16:25:02 -0300

    eliminar cambios originales para arrancar de cero

--- cmd/main.go
+++ cmd/main.go
@@ -1,65 +1,9 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
-	"os"
-	"sync"
-
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/dynamodb"
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/raviand/test-project/cmd/server"
-	"github.com/raviand/test-project/internal/audit"
-	"github.com/raviand/test-project/internal/controller"
-	"github.com/raviand/test-project/internal/repository"
-	"github.com/raviand/test-project/internal/service"
 )
 
 func main() {
-	fmt.Println("Starting server...")
-	mysql, err := InitDB()
-	if err != nil {
-		panic(err)
-	}
-	dynamo, err := InitDynamo()
-	if err != nil {
-		panic(err)
-	}
-	ddb := repository.NewDynamoRepository(dynamo, "User")
-	db := repository.NewDatabase(mysql)
-	svc := service.NewProductService(db, ddb)
-	var wg sync.WaitGroup
-	notiChannel, auditor := audit.NewAuditRoutine(&wg)
-	ctrl := controller.NewHandler(svc, notiChannel)
-	wg.Add(1)
-	go auditor.Run()
-	if err := server.NewServer(ctrl).Start(); err != nil {
-		panic(err)
-	}
-	close(notiChannel)
-	wg.Wait()
-}
-
-func InitDB() (*sql.DB, error) {
-	dsn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/movies_db", os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"))
-	mysql, err := sql.Open("mysql", dsn)
-	if err != nil {
-		panic(err)
-	}
-	return mysql, nil
-}
-
-func InitDynamo() (*dynamodb.DynamoDB, error) {
-	region := "us-west-2"
-	endpoint := os.Getenv("DYNAMO_ENDPOINT")
-	cred := credentials.NewStaticCredentials("local", "local", "")
-	sess, err := session.NewSession(aws.NewConfig().WithEndpoint(endpoint).WithRegion(region).WithCredentials(cred))
-	if err != nil {
-		return nil, err
-	}
-	dynamo := dynamodb.New(sess)
-	return dynamo, nil
+	fmt.Println("Hello, World!")
 }
--- go.mod
+++ go.mod
@@ -2,23 +2,6 @@ module github.com/raviand/test-project
 
 go 1.21.6
 
-require (
-	github.com/DATA-DOG/go-sqlmock v1.5.2
-	github.com/aws/aws-sdk-go v1.55.5
-	github.com/go-chi/chi/v5 v5.1.0
-	github.com/go-sql-driver/mysql v1.8.1
-	github.com/stretchr/testify v1.9.0
-	gorm.io/driver/mysql v1.5.7
-	gorm.io/gorm v1.25.11
-)
+require github.com/go-sql-driver/mysql v1.8.1
 
-require (
-	filippo.io/edwards25519 v1.1.0 // indirect
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/jinzhu/inflection v1.0.0 // indirect
-	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/jmespath/go-jmespath v0.4.0 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	golang.org/x/text v0.16.0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
-)
+require filippo.io/edwards25519 v1.1.0 // indirect
